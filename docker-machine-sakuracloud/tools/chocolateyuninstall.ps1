3ebf7cb947cbc89118d090c81ef63f38400103d2
223n <tatami@cmail.plala.or.jp>
2017-12-14 23:12:14 +0900

    Add uninstall test. Remove unused value.

--- docker-machine-sakuracloud/tools/chocolateyuninstall.ps1
+++ docker-machine-sakuracloud/tools/chocolateyuninstall.ps1
@@ -7,8 +7,6 @@ $packageArgs = @{
   zipFileName64 = 'docker-machine-driver-sakuracloud_windows-amd64.zip'
 }
 
-$uninstalled = $false
-
 # Only necessary if you did not unpack to package directory - see https://chocolatey.org/docs/helpers-uninstall-chocolatey-zip-package
 $os = Get-WmiObject -Class Win32_OperatingSystem;
 if ($os.OSarchitecture.Contains("64")) {
