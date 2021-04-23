c66ec2e8a4da429861db8427f47cd77d85f3ad1e
223n <223n@223n.tech>
2021-04-23 13:27:05 +0900

    Update chocolateyinstall.ps1
    ダウンロードURL変更対応

--- docker-machine-sakuracloud/tools/chocolateyinstall.ps1
+++ docker-machine-sakuracloud/tools/chocolateyinstall.ps1
@@ -4,9 +4,7 @@ $packageName  = $env:ChocolateyPackageName;
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)";
 $softwareName = 'docker-machine-sakuracloud*';
 $url32        = 'https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v___VERSION___/docker-machine-driver-sakuracloud___VERSION___windows_386.zip';
-#                https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v1.5.0        /docker-machine-driver-sakuracloud_1.5.0_windows_386.zip
-$url64        = 'https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v__VERSION__/docker-machine-driver-sakuracloud___VERSION___windows-amd64.zip';
-#                https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v1.5.0     /docker-machine-driver-sakuracloud_1.5.0_windows_amd64.zip
+$url64        = 'https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v__VERSION__/docker-machine-driver-sakuracloud___VERSION___windows_amd64.zip';
 $hashType     = 'sha512';
 $hash32       = '__HASH32__';
 $hash64       = '__HASH64__';
